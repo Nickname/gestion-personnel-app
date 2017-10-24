4439fc0c89188daa9cfec0aba9419e78647625f5
Nickname <niko.cavaille@hotmail.fr>
2017-10-24 13:50:37 +0200

    premier commit

---
+++ nav.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#bandeau").html(`
+    <div id='navigation' class='row'>
+      <nav class='col-md-12 col-sm-12'>
+        <ul class='nav nav-pills'>
+          <li class='nav-item'><a class='nav-link active' href='activite.html'>Collaborateurs</a></li>
+          <li class='nav-item'><a class='nav-link' href='statistique.html'>Statistiques</a></li>
+          <li class='nav-item'><a class='nav-link' href='activite.html'>Activités</a></li>
+      </nav>
+    </div>`
+  );
+})
